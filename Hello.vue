fe3f29026c6b81b2b7eb262bf2d1ff5f99d79c51
Hai Nguyen Le <nguyenlehai@hotmail.com>
2022-09-23 16:20:23 +0700

    Initial commit

---
+++ Hello.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>{{ greeting }} World!</p>
+
+    <VueDocPreview :docValue="text" :docType="type" :height="80" />
+  </div>
+</template>
+
+<script>
+import VueDocPreview from "./vueDocPrev.vue";
+
+export default {
+  components: {
+    VueDocPreview,
+  },
+  data: function () {
+    return {
+      greeting: "BYE..",
+      text: "https://static-1252421604.cos.ap-guangzhou.myqcloud.com/vdp.docx",
+      type: "office",
+    };
+  },
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
---
+++ docvue.vue
@@ -0,0 +1,18 @@
+<template>
+  <VueDocPreview :value="docValue" :type="docType" />
+</template>
+ 
+<script>
+import VueDocPreview from 'vue-doc-preview';
+export default {
+  name: "docvue",
+  components: {
+    VueDocPreview
+  }.
+  props:{
+    docValue : {type: String,},
+    docType : {type: String,},
+  }
+  
+}
+</script>
